cfad213d0b0ccd7d49210390f296acd49c6a9769
arpit0910 <65845755+arpit0910@users.noreply.github.com>
2022-10-08 22:06:50 +0530

    Add files via upload

--- resources/views/frontend/layouts/main.blade.php
+++ resources/views/frontend/layouts/main.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <title>Norda - Minimal eCommerce HTML Template</title>
+    <title>Ecommerce</title>
     <meta name="robots" content="noindex, follow" />
     <meta name="description" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -23,6 +23,16 @@
 <body>
     <div class="main-wrapper">
         @include('frontend.layouts.partials.header')
+        <div class="alert alert-danger alert-dismissible fade d-none" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="alert alert-success alert-dismissible fade d-none" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
         @yield('content')
         @include('frontend.layouts.partials.footer')
     </div>
@@ -32,6 +42,60 @@
     <script src="{{asset('assets/js/main.js')}}"></script>
     @stack('page-scripts')
     @stack('custom-js')
+
+    <script>
+        $('.add-to-cart-button').click(function() {
+            let productId = $(this).data('product_id');
+            $.ajax({
+                url: "{{route('cart.store')}}",
+                method: "POST",
+                data: {
+                    "_token": "{{csrf_token()}}",
+                    "product_id": productId,
+                    "quantity": '1'
+                },
+                success: function(response) {
+                    if (response.data == 'success') {
+                        let cartCount = parseInt($('.cart-hidden-count').val());
+                        $('.cart-count').html(cartCount + 1);
+                        $('.cart-hidden-count').val(cartCount + 1);
+                    } else {
+                        $('.alert-danger').addClass('show');
+                        $('.alert-danger').html(response.message);
+                        $('.alert-danger').removeClass('d-none');
+                    }
+                },
+                error: function(response) {
+
+                }
+            });
+        });
+        $('.add-to-wishlist-button').click(function() {
+            let productId = $(this).data('product_id');
+            $.ajax({
+                url: "{{route('wishlist.store')}}",
+                method: "POST",
+                data: {
+                    "_token": "{{csrf_token()}}",
+                    "product_id": productId,
+                },
+                success: function(response) {
+                    if (response.data == 'success') {
+                        let wishlistCount = parseInt($('.wishlist-hidden-count').val());
+                        $('.wishlist-count').html(wishlistCount + 1);
+                        $('.wishlist-hidden-count').val(wishlistCount + 1);
+                    } else {
+                        $('.alert-danger').addClass('show');
+                        $('.alert-danger').html(response.message);
+                        $('.alert-danger').removeClass('d-none');
+                    }
+                },
+                error: function(response) {
+
+                }
+            });
+        });
+    </script>
 </body>
 
 </html>
